4cddc8eb5f995656e97843f59e2e66298b42ffa0
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-05-29 14:54:29 +0530

    Update mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -4,18 +4,6 @@ date
 cal
 ls
 echo "bye bye 4"
-echo "bye bye 5"
-echo "bye bye6"
-
-echo "hyusaedjhwsj"
-
-ls
-echo "bye bye 4"
-echo "bye bye 5"
-echo "bye bye6"
-echo "bye bye7"
-
-echo "bye bye7"
 
 
 # Testing triggers
